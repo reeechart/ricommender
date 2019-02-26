66e89a63e6b4d7fcba34c4b47b441ef6bb18ae31
reeechart <ferdinandusrichard@yahoo.co.id>
2019-02-26 13:54:00 +0700

    add extraction options in music_extractor

--- README.md
+++ README.md
@@ -9,5 +9,15 @@ Multi-context Music Recommender System Project
 ## Required Folders
 *musics* with mp3 files inside
 
+## Extracting Features
+```
+python music_extractor.py <extraction_type> <folder_name> <csv_filename>
+```
+
+Args:
+- `extraction_type` with either *extract_music* or *extract_music_frame*
+- `folder_name` with the name of to-be-extracted folder
+- `csv_filename` with the file name of csv file
+
 ## Author
 Ferdinandus Richard
--- music_extractor.py
+++ music_extractor.py
@@ -2,6 +2,7 @@ import csv
 import eyed3
 import librosa
 import numpy as np
+import sys
 
 def load_editorial_metadata(audiofile):
     '''Loads an audio file and extract its editorial metadata
@@ -22,18 +23,7 @@ def load_editorial_metadata(audiofile):
 
     return audio.tag.title, audio.tag.artist, audio.tag.album
 
-def with_quote(string):
-    '''Adds quote to a string
-
-    Args:
-        string (string): to be quoted string
-
-    Returns:
-        string (string): quoted string
-    '''
-    return '"'+string+'"'
-
-def extract_content(directory):
+def extract_music_content(directory):
     '''Extracts mp3 metadata from a specified directory
 
     Args:
@@ -57,11 +47,6 @@ def extract_content(directory):
         music_metadata.append(title)
         music_metadata.append(artist)
         music_metadata.append(album)
-
-        # music_metadata.append(with_quote(file))
-        # music_metadata.append(with_quote(title))
-        # music_metadata.append(with_quote(artist))
-        # music_metadata.append(with_quote(album))
 
         wf, sr = librosa.load(file)
 
@@ -94,16 +79,91 @@ def extract_content(directory):
     
     return all_metadata
 
-def save_to_csv(data):
+def extract_music_frames(directory):
+    '''Extracts mp3 metadata by frame
+
+    Args:
+        directory (string): directory that contains mp3 files
+
+    Returns:
+        metadata ([string]): all frames metadata
+    '''
+    all_metadata = [['title', 'artist', 'mean_thirteen_first_mfcc', 'zcr', 'max_chroma']]
+
+    files = librosa.util.find_files(directory, ext='mp3')
+
+    for file in files:
+        print('Extracting ', file, '...')
+
+        title, artist, _ = load_editorial_metadata(file)
+
+        wf, sr = librosa.load(file)
+
+        mfcc = librosa.feature.mfcc(y=wf, sr=sr)
+        mfcc = np.mean(mfcc[:13], axis=0) # take the first 13 mfcc values
+        # print(mfcc.shape)
+
+        zcr = librosa.feature.zero_crossing_rate(y=wf)
+        zcr = np.mean(zcr, axis=0)
+        # print(zcr.shape)
+
+        chroma_stft = librosa.feature.chroma_stft(y=wf, sr=sr)
+        chroma_stft_max = np.argmax(chroma_stft, axis=0)
+        # print(chroma_stft_max.shape)
+
+        for i in range(len(mfcc)):
+            music_frame_metadata = []
+            music_frame_metadata.append(title)
+            music_frame_metadata.append(artist)
+            music_frame_metadata.append(mfcc[i])
+            music_frame_metadata.append(zcr[i])
+            music_frame_metadata.append(chroma_stft_max[i])
+
+            all_metadata.append(music_frame_metadata)
+    
+    return all_metadata
+
+def save_to_csv(data, csv_file):
     '''Saves data (list) to a csv file
 
     Args:
         data ([object]): list of metadata to be saved
     '''
-    with open('music_metadata.csv', 'w', newline='') as f:
+    with open(csv_file, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
+def exit_with_msg(msg):
+    '''Exit with a custom message
+
+    Args:
+        msg (string): exit message
+    '''
+    print(msg)
+    sys.exit()
+
+def check_arguments(argv):
+    '''Check arguments when running the program
+
+    Args:
+        argv ([string]): list of arguments
+    '''
+    if (len(argv) != 4):
+        exit_with_msg('Need 4 arguments to continue')
+    else:
+        extraction_type = sys.argv[1]
+        music_folder = sys.argv[2]
+        csv_file = sys.argv[3]
+        return extraction_type, music_folder, csv_file
+
+# Main program
 if __name__ == '__main__':
-    metadata = extract_content('musics')
-    save_to_csv(metadata)
+    extraction_type, music_folder, csv_file = check_arguments(sys.argv)
+    if (extraction_type == 'extract_music'):
+        metadata = extract_music_content(music_folder)
+        save_to_csv(metadata, csv_file)
+    elif (extraction_type == 'extract_music_frame'):
+        metadata = extract_music_frames(music_folder)
+        save_to_csv(metadata, csv_file)
+    else:
+        exit_with_msg('Extraction type invalid, please use only \'extract_music\' or \'extract_music_frame\'')
